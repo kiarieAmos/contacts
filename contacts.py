6e180750f69b42c205d7931c4c2908d5cfb55157
kiarieAmos <amosmkiarie@gmail.com>
2025-04-30 20:53:37 +0300

    created the contacts project

---
+++ contacts.py
@@ -0,0 +1,3 @@
+### Importing all the needed modules from tkinter
+from tkinter import ttk
+from tkinter import Tk, Button, Label, PhotoImage, LabelFrame, W, E, N, S,Entry, END, StringVar, Scrollbar, Toplevel
